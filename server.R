c7480978ebf29990d21d25d8af20cfb9ffe7b4a4
Mike Russell <mike.russell@gov.ab.ca>
2017-12-15 14:53:58 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,10 +1,10 @@
 #install and load required packages -----------------
-ipak <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg))
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
+#ipak <- function(pkg){
+ # new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+ # if (length(new.pkg))
+#    install.packages(new.pkg, dependencies = TRUE)
+#  sapply(pkg, require, character.only = TRUE)
+#}
 
 # usage
 packages <- c("shiny", "RODBC","dplyr","Distance",
