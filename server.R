b75eb149fae4b0b6dba6f96460c213bc9d86f0fa
Mike Russell <mike.russell@gov.ab.ca>
2017-12-15 15:24:26 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,19 +1,19 @@
-#install and load required packages -----------------
-#ipak <- function(pkg){
- # new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
- # if (length(new.pkg))
-#    install.packages(new.pkg, dependencies = TRUE)
-#  sapply(pkg, require, character.only = TRUE)
-#}
+install and load required packages -----------------
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
 
 # usage
 packages <- c("shiny", "RODBC","dplyr","Distance",
   "mrds", "ggplot2", "rgdal",
   "rgeos","dsm","knitr","maptools","gridExtra","sp", "DT")
 
-sapply(packages, require, character.only = T)
+#sapply(packages, require, character.only = T)
 
-#ipak(packages)
+ipak(packages)
 
 ui <- fluidPage(
     verbatimTextOutput(("debug"))
